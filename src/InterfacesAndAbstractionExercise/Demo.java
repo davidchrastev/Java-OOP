067de9da8d39c00e6178d812204a1c5dbd63f67c
davidchrastev <106105745+davidchrastev@users.noreply.github.com>
2022-11-05 14:38:50 +0200

    Interfaces exercise

---
+++ src/InterfacesAndAbstractionExercise/Demo.java
@@ -0,0 +1,7 @@
+package InterfacesAndAbstractionExercise;
+
+public class Demo {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/InterfacesAndAbstractionExercise/interfacePerson/Person.java
@@ -0,0 +1,6 @@
+package InterfacesAndAbstractionExercise.interfacePerson;
+
+public interface Person {
+    String getName();
+    int getAge();
+}
